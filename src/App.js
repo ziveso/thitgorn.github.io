df04d05dd5ed19285bef6cf6b9ca11c334a21b25
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2017-12-08 16:36:40 +0700

    add some bootstrap setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Welcome from './components/Welcome'
 import About from './components/About'
 import Loading from './components/Loading'
