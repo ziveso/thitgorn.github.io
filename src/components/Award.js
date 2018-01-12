a0506673b962bfb855474e823a04b76fb0506813
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2018-01-13 02:45:50 +0700

    update

--- src/components/Award.js
+++ src/components/Award.js
@@ -3,16 +3,41 @@ import localhackday1 from '../images/localhackday1.jpg'
 import localhackday2 from '../images/localhackday2.jpg'
 import localhackday3 from '../images/localhackday3.png'
 import localhackday4 from '../images/localhackday4.jpg'
+import './popup.css'
 
 class Award extends React.Component {
+  constructor() {
+    super()
+    this.state = { toggle:"" }
+    this.clickLHD = this.clickLHD.bind(this);
+  }
+
+  componentDidUpdate() {
+    console.log(this.state);
+  }
+
+  getPopup() {
+    if(this.state.toggle === "LHD") {
+      const img = <div className='popup'>
+                    <img src={localhackday3} className="img-responsive" alt="localhackday3"/>
+                    <img src={localhackday1} className="img-responsive" alt="localhackday1"/>
+                    <img src={localhackday2} className="img-responsive" alt="localhackday2"/>
+                    <img src={localhackday4} className="img-responsive" alt="localhackday4"/>
+                  </div>
+      return img;
+    }
+    return null;
+  }
+
+  clickLHD() {
+    this.setState( { toggle:"LHD" } );
+  }
+
   render () {
     return (<div>
               <h1>AWARD / ACHIEVEMENT</h1>
-              <div>1st LOCAL HACKDAY - Hackaton 2017</div>
-              <img src={localhackday3} className="img-responsive" alt="localhackday3"/>
-              <img src={localhackday1} className="img-responsive" alt="localhackday1"/>
-              <img src={localhackday2} className="img-responsive" alt="localhackday2"/>
-              <img src={localhackday4} className="img-responsive" alt="localhackday4"/>
+              <div onClick={this.clickLHD}>1st LOCAL HACKDAY - Hackaton 2017</div>
+              <div>{this.getPopup()}</div>
             </div>);
   }
 }
