32b352a2a99f14bec6c21487caa441c907fa0ce9
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2018-02-13 16:32:43 +0700

    :lipstick: add terminal theme

---
+++ src/components/Welcome/Command.js
@@ -0,0 +1,14 @@
+const cmd = {
+  "ls" : "README.txt",
+  "cat README.txt" : "Portfolio website\n
+                      Thitiwat Thongbor - SOFTWARE DEVELOPER\n\n
+                      see avliable command say `command`",
+  "clear" : this.clear(),
+  "next" : this.next(),
+  "about" : "",
+  "experience" : "",
+  "award" : "",
+  "contact" : "",
+}
+
+export default cmd
--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -2,12 +2,27 @@ import React from 'react'
 import './Welcome.css'
 import { scroller } from 'react-scroll'
 
-
 var style = {
   display: 'flex' ,
-    };
+};
+
+const USERNAME = "user $:"
+const Greeting = "login as unknown user\n\ncat README.txt\n"
+const README =
+`Portfolio website\nThitiwat Thongbor - SOFTWARE DEVELOPER\n\n
+see command run cmd\n=============\n`
 
 class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: Greeting + README + USERNAME};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleCommand = this.handleCommand.bind(this);
+  }
+
+
   scroll() {
     scroller.scrollTo('ABOUT', {
       duration: 1000,
@@ -16,17 +31,61 @@ class Welcome extends React.Component {
     })
   }
 
+  handleChange(event) {
+    if (event.target.value.substring(event.target.value.length-6 , event.target.value.length)==='user $')
+      event.target.value=event.target.value+":"
+    var inputChar = event.target.value.charAt(event.target.value.length-1)
+    this.setState({value: event.target.value})
+    if(inputChar==='\n') {
+      var cmd = this.getCommand()
+      if(cmd==="Enter") {
+        this.setState({value: event.target.value + USERNAME})
+      } else if(cmd=="clear"){
+        this.setState({value: USERNAME})
+      } else {
+        var output = this.handleCommand(cmd)
+        this.setState({value: event.target.value + output + "\n" + USERNAME})
+      }
+    }
+  }
+
+  getCommand() {
+    var s = this.state.value.split(USERNAME)
+    var a = s[s.length - 1]
+    a = a.trim()
+    if(a=="")
+      return "Enter"
+    return a
+  }
+
+  handleCommand(cmd) {
+    var output = ""
+    console.log(cmd);
+    switch(cmd) {
+      case "Enter": break;
+      case "next": output = "next()"; this.scroll(); break;
+      case "whoami": output = "Thitiwat Thongbor"; break;
+      case "clear": output = "clear";break;
+      default: output = "command not found: " + cmd;
+    }
+    return output
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+
   render() {
     return (<div className="welcome">
-                <div className="header">
-                  <h1 style={style}>
-                    <strong>Hi, I'm Thitiwat Thongbor.</strong>
-                  </h1>
-                    SOFTWARE AND KNOWLEDGE ENGINEERING
-                </div>
-                <div className="abc" onClick={this.scroll}>
-                  <div className="arrow bounce"></div>
-                </div>
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                    <textarea type="text" value={this.state.value}
+                    onChange={this.handleChange}
+                    style={{background:`black`,color:`green`,width:`100%`,height:`100vh`}}>
+                    </textarea>
+                    </div>
+                </form>
             </div>)
   }
 }
