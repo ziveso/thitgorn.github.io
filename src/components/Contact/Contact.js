c347356d3c89676a6e641461db0a076382d39cce
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2018-02-14 18:14:52 +0700

    :bug: fix arrow not found

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
-class Contact extends React.Component {
-  render() {
-    return (<div>
-              <h1>CONTACT</h1>
-              tell num
-              mail
-              github
-              address
-            </div>)
-  }
-}
+// class Contact extends React.Component {
+//   render() {
+//     return (<div>
+//               <h1>CONTACT</h1>
+//               tell num
+//               mail
+//               github
+//               address
+//             </div>)
+//   }
+// }
 
 class Notuse extends React.Component {
   render() {
