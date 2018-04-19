e29e6f7a64ee80b17efb29bff0bb2b148a264d78
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2018-04-19 15:44:51 +0700

    use moment js instead of typing update date

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,10 +3,11 @@ import './Contact.css'
 import FACEBOOK from '../../images/icon/facebook.png'
 import TWITTER from '../../images/icon/twitter.png'
 import INSTAGRAM from '../../images/icon/instagram.png'
-import time from '../../updated'
+import Moment from 'moment'
 
 class Contact extends React.Component {
   render() {
+    var date = Moment()
     return (<div className='Contact'>
               <h5>Want to hire me? </h5>
               <h5>Email me at <a href="mailto:thitiwat.tho1@ku.th?Subject=I%20found%20you%20on%20github%20page">thitiwat.tho1@ku.th</a></h5>
@@ -18,7 +19,7 @@ class Contact extends React.Component {
               <h6>or Call +668-377-0808</h6>
               <h5>You'll find a new technology world!</h5>
               <div>
-                last update : {time.date}
+                last update : {date.format("MMM Do YY")}
               </div>
             </div>)
   }
--- src/updated.js
+++
@@ -1,5 +0,0 @@
-const updated = {
-  date : "4 MAY 2018"
-}
-
-export default updated;
